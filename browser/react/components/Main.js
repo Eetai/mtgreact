88bd231ae0855bca0f0b27328c3ad302a59207eb
Eetai Ben-Sasson <eeetai@gmail.com>
2017-12-03 18:29:12 -0500

    early

---
+++ browser/react/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+
+
+export default class Main extends Component {
+    render() {
+        return ( <
+            div >
+            Hello World ReactJS!!!
+            <
+            /div>
+        );
+
+
+
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var config = {
+    entry: './browser/react/index.js', // entry point
+    output: {
+        path: __dirname,
+        filename: 'index.js', // place where bundled app will be served
+    },
+    context: __dirname,
+    devtool: 'source-map',
+    module: {
+        loaders: [{
+            test: /jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader'
+        }]
+    }
+};
+
+module.exports = config;
